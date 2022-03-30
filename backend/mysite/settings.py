3cdfeb4f98e5e630ca41b1cf16530adfe4bdc643
Karolina Ścibek <32667040+karolinascibek@users.noreply.github.com>
2022-03-30 08:25:57 +0200

    Dodanie połaczenia z bazą danych
    Dodanie ustawień w plikach settings.py danych bazy(MYSQL) do połaczenia.

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zai_polls',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
