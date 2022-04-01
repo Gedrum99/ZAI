1cf2438ba3ae97d20c5a0e3be9906dfb5d403013
Robert Żulewski <66266542+Gedrum99@users.noreply.github.com>
2022-04-01 20:42:57 +0200

    Dodanie modeli

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -3,8 +3,42 @@ from django.contrib.auth.backends import get_user_model
 
 # Create your models here.
 
+class Respondend(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+
+    def __str__(self):
+        return "Respondend no. "+self.id
+
+class Question_type(models.Model):
+    name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.name
+
 class Poll(models.Model):
     title = models.CharField(max_length=100)
-    description = models.TextField()
-    #author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    description = models.CharField(max_length=250)
+    user_id = models.ForeignKey(User,related_name="users",on_delete=models.SET_NULL,null=True)
 
+    def __str__(self):
+        return self.title + ' '+self.description
+
+class Respondend_answer(models.Model):
+    respondend_id = models.ForeignKey(Respondend, related_name="respondends", on_delete=models.SET_NULL,null=True)
+    poll_id = models.ForeignKey(Poll, related_name="polls", on_delete=models.SET_NULL, null=True)
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    poll_id = models.ForeignKey(Poll,related_name="polls", on_delete=models.SET_NULL,null=True)
+    question_type_id =models.ForeignKey(Question_type, related_name="question_types", on_delete=models.SET_NULL, null=True)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    text = models.CharField(max_length=250)
+    question_id = models.ForeignKey(Question,related_name="questions",on_delete=models.SET_NULL,null=True)
+
+    def __str__(self):
+        return self.text
