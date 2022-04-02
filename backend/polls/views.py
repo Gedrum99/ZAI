123054a5a4b85bc2fc6443127159d2e02b506901
Karolina Ścibek <32667040+karolinascibek@users.noreply.github.com>
2022-04-02 15:00:57 +0200

    Dodanie widoku QuestionTypeList

--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 from rest_framework import views
+from rest_framework import generics
+from .serializers import QuestionTypeSerializer
+from .models import QuestionType
 
 # Create your views here.
+
+
+class QuestionTypeList(generics.ListCreateAPIView):
+    queryset = QuestionType.objects.all()
+    serializer_class = QuestionTypeSerializer
+    name = 'questiontype-list'
