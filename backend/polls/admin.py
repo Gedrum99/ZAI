0a725e6322b2216de507ed3b78baad042dfdc139
Karolina Ścibek <32667040+karolinascibek@users.noreply.github.com>
2022-04-02 15:01:35 +0200

    Dodanie do panelu admina model QuestionType

--- backend/polls/admin.py
+++ backend/polls/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import QuestionType
 
 # Register your models here.
+admin.site.register(QuestionType)
--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     # path('polls/', include('polls.urls')),
+    path('question-type', views.QuestionTypeList.as_view(), name=views.QuestionTypeList.name),
 ]
