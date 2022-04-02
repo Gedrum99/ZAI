1e9ab5a420da651d93a4a37fa3e496311c76caa3
Karolina Ścibek <32667040+karolinascibek@users.noreply.github.com>
2022-04-02 14:02:59 +0200

    Poprawienie modeli w app polls/models
    Usunięcie z nazw atrybótów końcówki _id, poniważ dublowała się po zmigorwaniu do bazy.

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.backends import get_user_model
+User = get_user_model()
 
 # Create your models here.
 
+
 class Respondend(models.Model):
-    id = models.PositiveIntegerField(primary_key=True)
+    user = models.ForeignKey(User, related_name="respondents", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
-        return "Respondend no. "+self.id
+        return "Respondend no. "+self.user
+
 
 class Question_type(models.Model):
     name = models.CharField(max_length=70)
@@ -15,30 +18,34 @@ class Question_type(models.Model):
     def __str__(self):
         return self.name
 
+
 class Poll(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
-    user_id = models.ForeignKey(User,related_name="users",on_delete=models.SET_NULL,null=True)
+    user = models.ForeignKey(User, related_name="polls", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.title + ' '+self.description
 
+
 class Respondend_answer(models.Model):
-    respondend_id = models.ForeignKey(Respondend, related_name="respondends", on_delete=models.SET_NULL,null=True)
-    poll_id = models.ForeignKey(Poll, related_name="polls", on_delete=models.SET_NULL, null=True)
+    respondend = models.ForeignKey(Respondend, related_name="respondends", on_delete=models.SET_NULL, null=True)
+    poll = models.ForeignKey(Poll, related_name="polls", on_delete=models.SET_NULL, null=True)
+
 
 class Question(models.Model):
     title = models.CharField(max_length=100)
-    poll_id = models.ForeignKey(Poll,related_name="polls", on_delete=models.SET_NULL,null=True)
-    question_type_id =models.ForeignKey(Question_type, related_name="question_types", on_delete=models.SET_NULL, null=True)
+    poll = models.ForeignKey(Poll, related_name="questions", on_delete=models.SET_NULL, null=True)
+    question_type =models.ForeignKey(Question_type, related_name="questions", on_delete=models.SET_NULL, null=True)
     number = models.IntegerField()
 
     def __str__(self):
         return self.title
 
+
 class Answer(models.Model):
     text = models.CharField(max_length=250)
-    question_id = models.ForeignKey(Question,related_name="questions",on_delete=models.SET_NULL,null=True)
+    question = models.ForeignKey(Question, related_name="answers", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.text
