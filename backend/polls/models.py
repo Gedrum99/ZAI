fc8cc75d9e2c34ea81320396be107bf466a17b22
Karolina Ścibek <32667040+karolinascibek@users.noreply.github.com>
2022-04-02 14:31:06 +0200

    Poprawienie nazw class w models

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -5,20 +5,6 @@ User = get_user_model()
 # Create your models here.
 
 
-class Respondend(models.Model):
-    user = models.ForeignKey(User, related_name="respondents", on_delete=models.SET_NULL, null=True)
-
-    def __str__(self):
-        return "Respondend no. "+self.user
-
-
-class Question_type(models.Model):
-    name = models.CharField(max_length=70)
-
-    def __str__(self):
-        return self.name
-
-
 class Poll(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
@@ -28,15 +14,17 @@ class Poll(models.Model):
         return self.title + ' '+self.description
 
 
-class Respondend_answer(models.Model):
-    respondend = models.ForeignKey(Respondend, related_name="respondends", on_delete=models.SET_NULL, null=True)
-    poll = models.ForeignKey(Poll, related_name="polls", on_delete=models.SET_NULL, null=True)
+class QuestionType(models.Model):
+    name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return self.name
 
 
 class Question(models.Model):
     title = models.CharField(max_length=100)
     poll = models.ForeignKey(Poll, related_name="questions", on_delete=models.SET_NULL, null=True)
-    question_type =models.ForeignKey(Question_type, related_name="questions", on_delete=models.SET_NULL, null=True)
+    question_type =models.ForeignKey(QuestionType, related_name="questions", on_delete=models.SET_NULL, null=True)
     number = models.IntegerField()
 
     def __str__(self):
@@ -49,3 +37,16 @@ class Answer(models.Model):
 
     def __str__(self):
         return self.text
+
+
+class Respondent(models.Model):
+    user = models.ForeignKey(User, related_name="respondents", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return "Respondend no. "+self.user
+
+
+class RespondentAnswer(models.Model):
+    respondent = models.ForeignKey(Respondent, related_name="respondends", on_delete=models.SET_NULL, null=True)
+    poll = models.ForeignKey(Poll, related_name="polls", on_delete=models.SET_NULL, null=True)
+
