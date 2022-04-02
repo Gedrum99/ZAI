fe9a55891b1efcd1908c86762a720fd7df450a39
Karolina Ścibek <32667040+karolinascibek@users.noreply.github.com>
2022-04-02 14:37:55 +0200

    Dodanie class do pliku serializers.py
    Dodanie serializerów dla modeli Poll, QuestionType, Question

--- backend/polls/serializers.py
+++ backend/polls/serializers.py
@@ -1,1 +1,20 @@
 from rest_framework import serializers
+from .models import Poll, QuestionType, Question
+
+
+class PollSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Poll
+        fields = ['id', 'title', 'description', 'user']
+
+
+class QuestionTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuestionType
+        fields = ['id', 'name']
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['id', 'title', 'poll', 'question_type', 'number']
