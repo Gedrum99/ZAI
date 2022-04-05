f1ca9d99181240f31cfbf1d893db57f816610a1b
Karolina Ścibek <32667040+karolinascibek@users.noreply.github.com>
2022-04-05 20:01:39 +0200

    Dodanie widoków dla Question i Polls

--- backend/polls/urls.py
+++ backend/polls/urls.py
@@ -5,5 +5,12 @@ from . import views
 
 urlpatterns = [
     # path('polls/', include('polls.urls')),
+    path('', views.PollList.as_view(), name=views.PollList.name),
+    path('<int:pk>', views.PollDetail.as_view(), name=views.PollDetail.name),
+
     path('question-type', views.QuestionTypeList.as_view(), name=views.QuestionTypeList.name),
+    path('question-type/<int:pk>', views.QuestionTypeDetail.as_view(), name=views.QuestionTypeDetail.name),
+
+    path('question', views.QuestionList.as_view(), name=views.QuestionList.name),
+    path('question/<int:pk>', views.QuestionDetail.as_view(), name=views.QuestionDetail.name),
 ]
--- backend/polls/views.py
+++ backend/polls/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework import views
 from rest_framework import generics
-from .serializers import QuestionTypeSerializer
-from .models import QuestionType
+from .serializers import QuestionTypeSerializer, PollSerializer, QuestionSerializer
+from .models import QuestionType, Poll, Question
 
 # Create your views here.
 
@@ -11,3 +11,33 @@ class QuestionTypeList(generics.ListCreateAPIView):
     queryset = QuestionType.objects.all()
     serializer_class = QuestionTypeSerializer
     name = 'questiontype-list'
+
+
+class QuestionTypeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = QuestionType.objects.all()
+    serializer_class = QuestionTypeSerializer
+    name = 'questiontype-detail'
+
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    name = 'polls-list'
+
+
+class PollDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+    name = 'poll-detail'
+
+
+class QuestionList(generics.ListCreateAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    name = 'question-list'
+
+
+class QuestionDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    name = 'question-detail'
