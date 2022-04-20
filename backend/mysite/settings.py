e9b0f53fd761f507629b37ba99a11d11912e4e14
Karolina Ścibek <32667040+karolinascibek@users.noreply.github.com>
2022-04-20 09:06:32 +0200

    Dodanie globalnej paginacji

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 2,
+
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
